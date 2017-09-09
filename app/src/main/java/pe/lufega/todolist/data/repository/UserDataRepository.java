1e74304d4b055f07482faf4cb91b10790baae4ca
Franco Cabanillas <fcabanillas@grupotsiperu.com.pe>
2017-09-09 12:50:49 -0500

    Presentador

---
+++ app/src/main/java/pe/lufega/todolist/data/repository/UserDataRepository.java
@@ -0,0 +1,8 @@
+package pe.lufega.todolist.data.repository;
+
+/**
+ * Created by DesarrolloF on 07/09/2017.
+ */
+
+public class UserDataRepository {
+}
---
+++ app/src/main/java/pe/lufega/todolist/data/repository/datasource/UserDataSourceFactory.java
@@ -0,0 +1,8 @@
+package pe.lufega.todolist.data.repository.datasource;
+
+/**
+ * Created by DesarrolloF on 07/09/2017.
+ */
+
+public class UserDataSourceFactory {
+}
